0576715f93c1b0281c26285f960685d3461f8fdc
diegoddn <dhiego.natividade@hotmail.com>
2025-11-11 17:46:23 -0300

    Iniciando o Git

---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    const elemento = document.getElementById("mensagem");
+    elemento.textContent =
+        "Você clicou no botão! Agora pratique enviar esta mudança para o GitHub.";
+}
