e9a0d076ac04a6a26b514da940f57e8ffa0fed52
diegoddn <dhiego.natividade@hotmail.com>
2025-11-11 18:13:34 -0300

    Alterando o texto da função para o texto original

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 function mostrarMensagem() {
     const elemento = document.getElementById("mensagem");
     elemento.textContent =
-        "Você clicou no botão! Agora pratique enviar esta mudança para o Git e GitHub.";
+        "Você clicou no botão! Agora pratique enviar esta mudança para o GitHub.";
 }
