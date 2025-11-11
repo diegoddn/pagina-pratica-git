7ff02849e8beeb2f7b6af9bf9421930912653b51
diegoddn <dhiego.natividade@hotmail.com>
2025-11-11 17:59:40 -0300

    Alterando o texto do primeiro parágrafo do main

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-function mostrarMensagem() {
+function() {
     const elemento = document.getElementById("mensagem");
     elemento.textContent =
         "Você clicou no botão! Agora pratique enviar esta mudança para o GitHub.";
